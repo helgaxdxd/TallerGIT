af6c31ec84c439eaf7d46f46bbee85008b17a7bd
Elia E <eliaemb33@gmail.com>
2023-11-22 17:46:14 -0600

    Cambiamos el titulo de la grafica

--- grafica.py
+++ grafica.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 plt.style.use('_mpl-gallery')
-
+plt.title('Esta es una gráfica')
 # make data:
 x = 0.5 + np.arange(8)
 y = [4.8, 5.5, 3.5, 4.6, 6.5, 6.6, 2.6, 3.0]
